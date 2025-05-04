515f99f5902cec3652de2a69e33684c1bee46648
BHlmaoMSD <92434431+BHlmaoMSD@users.noreply.github.com>
2025-05-04 16:14:17 +0700

    Thêm thông báo để viết đã chuyển sang bộ gõ nào

--- waybar/scripts/ibus.sh
+++ waybar/scripts/ibus.sh
@@ -3,8 +3,10 @@ status=$(ibus engine | grep BambooUs)
 if [ -n "$status" ]
 then
    ibus engine Bamboo >> /dev/null
+   notify-send "language Input" "Changed to VN" -i /usr/share/ibus-bamboo/icons/vi.svg -t 1000 -r 1006
 else
    ibus engine BambooUs >> /dev/null
+   notify-send "language Input" "Changed to US" -i /usr/share/ibus-bamboo/icons/us.svg -t 1000 -r 1006
 fi
 
 ## Nếu các ông dùng cái này thì chọn phương thức nhập là Bamboo Và BambooUS nha
